8daa7ad1c676a318ffe02c9dbc065886a6f4251f
Oliver Nordbjerg <hi@notbjerg.me>
2024-07-01 15:44:40 +0200

    chore: revm changes

--- crates/primitives/src/revm/env.rs
+++ crates/primitives/src/revm/env.rs
@@ -6,6 +6,7 @@ use crate::{
 use reth_chainspec::{Chain, ChainSpec};
 
 use alloy_eips::{eip4788::BEACON_ROOTS_ADDRESS, eip7002::WITHDRAWAL_REQUEST_PREDEPLOY_ADDRESS};
+use revm_primitives::AuthorizationList;
 #[cfg(feature = "optimism")]
 use revm_primitives::OptimismFields;
 
@@ -270,14 +271,7 @@ where
             tx_env.data = tx.input.clone();
             tx_env.chain_id = Some(tx.chain_id);
             tx_env.nonce = Some(tx.nonce);
-            tx_env.access_list = tx
-                .access_list
-                .0
-                .iter()
-                .map(|l| {
-                    (l.address, l.storage_keys.iter().map(|k| U256::from_be_bytes(k.0)).collect())
-                })
-                .collect();
+            tx_env.access_list = tx.access_list.clone().into();
             tx_env.blob_hashes.clear();
             tx_env.max_fee_per_blob_gas.take();
         }
@@ -293,14 +287,7 @@ where
             tx_env.data = tx.input.clone();
             tx_env.chain_id = Some(tx.chain_id);
             tx_env.nonce = Some(tx.nonce);
-            tx_env.access_list = tx
-                .access_list
-                .0
-                .iter()
-                .map(|l| {
-                    (l.address, l.storage_keys.iter().map(|k| U256::from_be_bytes(k.0)).collect())
-                })
-                .collect();
+            tx_env.access_list = tx.access_list.clone().into();
             tx_env.blob_hashes.clear();
             tx_env.max_fee_per_blob_gas.take();
         }
@@ -313,14 +300,7 @@ where
             tx_env.data = tx.input.clone();
             tx_env.chain_id = Some(tx.chain_id);
             tx_env.nonce = Some(tx.nonce);
-            tx_env.access_list = tx
-                .access_list
-                .0
-                .iter()
-                .map(|l| {
-                    (l.address, l.storage_keys.iter().map(|k| U256::from_be_bytes(k.0)).collect())
-                })
-                .collect();
+            tx_env.access_list = tx.access_list.clone().into();
             tx_env.blob_hashes.clone_from(&tx.blob_versioned_hashes);
             tx_env.max_fee_per_blob_gas = Some(U256::from(tx.max_fee_per_blob_gas));
         }
@@ -328,35 +308,14 @@ where
             tx_env.gas_limit = tx.gas_limit;
             tx_env.gas_price = U256::from(tx.max_fee_per_gas);
             tx_env.gas_priority_fee = Some(U256::from(tx.max_priority_fee_per_gas));
-            tx_env.transact_to = match tx.to {
-                TxKind::Call(to) => TransactTo::Call(to),
-                TxKind::Create => TransactTo::create(),
-            };
+            tx_env.transact_to = tx.to.into();
             tx_env.value = U256::ZERO;
             tx_env.data = tx.input.clone();
             tx_env.chain_id = Some(tx.chain_id);
             tx_env.nonce = Some(tx.nonce);
-            tx_env.access_list = tx
-                .access_list
-                .0
-                .iter()
-                .map(|l| {
-                    (l.address, l.storage_keys.iter().map(|k| U256::from_be_bytes(k.0)).collect())
-                })
-                .collect();
-            tx_env.authorization_list = tx
-                .authorization_list
-                .0
-                .iter()
-                .map(|a| revm_primitives::Authorization {
-                    chain_id: a.chain_id,
-                    address: a.address,
-                    nonce: a.nonce,
-                    y_parity: a.y_parity,
-                    r: a.r,
-                    s: a.s,
-                })
-                .collect();
+            tx_env.access_list = tx.access_list.clone().into();
+            tx_env.authorization_list =
+                Some(AuthorizationList::Signed(tx.authorization_list.clone()));
             tx_env.blob_hashes.clear();
             tx_env.max_fee_per_blob_gas.take();
         }
